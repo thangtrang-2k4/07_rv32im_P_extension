2e8af4849146e87e01224ec2d49b1e31c7943b09
thangtrang-2k4 <thangtdv2004@gmail.com>
2026-04-19 23:20:24 +0700

    pext

--- tb/tb_rv32imp_pipeline.sv
+++ tb/tb_rv32imp_pipeline.sv
@@ -37,7 +37,7 @@ module tb_rv32imp_pipeline;
 
   localparam int depth = 50;
   localparam int BaseAddr = 32'h80010000;
-  localparam int OAddr = 32'h80010000;
+  localparam int OAddr = 32'h800100e8;
 
   initial begin
     logic [31:0] golden [depth];
@@ -45,17 +45,17 @@ module tb_rv32imp_pipeline;
     int error;
 
     #10;
-    load_imem("../sw/Filter-Fir/scala_imem.hex");
-    load_dmem("../sw/Filter-Fir/scala_dmem.hex");
+    load_imem("../sw/Filter-Fir/pext_imem.hex");
+    load_dmem("../sw/Filter-Fir/pext_dmem.hex");
 
-    load_golden("../sw/Filter-Fir/scala_goldenw.hex", golden);
+    load_golden("../sw/Filter-Fir/pext_goldenw.hex", golden);
 
     // Chờ cho cờ done_flag = 1 từ file scala.c đánh dấu kết thúc
     wait (done == 1'b1);
     #20;
 
-    dump_result(depth, BaseAddr, OAddr, "../sw/Filter-Fir/scala_signature.hex");
-    load_result("../sw/Filter-Fir/scala_signature.hex", result);
+    dump_result(depth, BaseAddr, OAddr, "../sw/Filter-Fir/pext_signature.hex");
+    load_result("../sw/Filter-Fir/pext_signature.hex", result);
     #1;
     compare_result(depth, OAddr, golden, result, error);
     if (error == 0)
